1c8ed8ce1aa7ce2859a6b30e40cdcc9eaa27572b
SidhantNaik <mr.sidhantnaik@gmail.com>
2025-08-28 19:52:09 +0530

    Corrected edit message functionality with method-override package.
    Integrated the method-override package to support HTTP PUT for editing chat messages.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
 const Chat = require("./models/chat.js")
 
 const app = express();
@@ -10,42 +11,45 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/ChattingApp')
     .then(() => { console.log("Database is connected."); })
     .catch(ex => { console.log(ex); });
 
-
+//Home route to display all messages
 app.get('/chats', async (req, res) => {
     const chats = await Chat.find();
     res.render('home', { chats });
 })
 
-
+//Show insert form
 app.get('/chat/new', (req, res) => {
     res.render("insert");
 })
 
+//Insert new message
 app.post('/chats', async (req, res) => {
     const data = req.body;
     data.created_at = new Date();
-    await Chat.insertOne(data)
+    await Chat.create(data); 
     res.redirect('/chats');
 })
 
+//Show edit form
 app.get('/chat/:id/edit', async (req, res) => {
     const { id } = req.params;
     const chat = await Chat.findById(id);
     res.render("edit", { chat });
 })
 
-app.put('/chat', async (req, res) => {
-    const data = req.body;
-    data.created_at = new Date();
-    await Chat.findByIdAndUpdate(data);
+//Edit the message
+app.put('/chats/:id', async (req, res) => {
+    const { id } = req.params;
+    const { msg } = req.body;
+    await Chat.findByIdAndUpdate(id, { msg:msg });
     res.redirect('/chats');
-    alert("Message Edited Succesfully.");
 })
 
 
