85c6d6bb4eaaf30b54b9c6d7da5b0e6bd92a831f
SidhantNaik <mr.sidhantnaik@gmail.com>
2025-08-29 18:34:46 +0530

    Add delete chat functionality
    Introduced a DELETE route in index.js to allow deletion of chat messages by ID. Updated home.ejs to include a delete button .

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/ChattingApp')
     .then(() => { console.log("Database is connected."); })
     .catch(ex => { console.log(ex); });
 
+
+
 //Home route to display all messages
 app.get('/chats', async (req, res) => {
     const chats = await Chat.find();
@@ -52,7 +54,12 @@ app.put('/chats/:id', async (req, res) => {
     res.redirect('/chats');
 })
 
-
+//Delete Route
+app.delete('/chat/:id', async (req, res) => {
+    const {id} = req.params;
+    await Chat.findByIdAndDelete(id);
+    res.redirect('/chats');
+})
 
 app.listen(3000, () => {
     console.log("Server is running at port : 3000");
